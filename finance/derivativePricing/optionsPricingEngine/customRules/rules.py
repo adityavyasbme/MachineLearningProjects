f9e8a01a187c6bd97b5b7eb8c6da63da2c704e7a
adityavyasbme <adityavyas1603@gmail.com>
2023-10-23 00:40:03 -0500

    added gamma, rho, vega rules

--- finance/derivativePricing/optionsPricingEngine/customRules/rules.py
+++ finance/derivativePricing/optionsPricingEngine/customRules/rules.py
@@ -1,6 +1,9 @@
 from optionsPricingEngine.customRules.intrinsicRules import rules as irl
 from optionsPricingEngine.customRules.timeDecayRules import rules as tdr
 from optionsPricingEngine.customRules.deltaRules import rules as dr
+from optionsPricingEngine.customRules.gammaRules import rules as gr
+from optionsPricingEngine.customRules.vegaRules import rules as vr
+from optionsPricingEngine.customRules.rhoRules import rules as rr
 
 
-rules = irl + tdr + dr
+rules = irl + tdr + dr + gr + vr + rr
